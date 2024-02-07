b06368930f4716fb4d6c2e222b3d540c63ba6995
kamel Benstaali <kamel.benstaali@yahoo.com>
2024-02-07 14:38:18 +0100

    adding  comment

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" />
+      {/* Using bloglist component again */}
       <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
     </div>
   );
