de4c87499e9834c0ecbf23f9a019ae459600920d
kamel Benstaali <kamel.benstaali@yahoo.com>
2024-02-07 15:04:03 +0100

    useEffects dependencies

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -21,6 +23,7 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
     </div>
   );
 }
